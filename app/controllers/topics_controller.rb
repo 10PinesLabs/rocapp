c40467bce8df6efdf83fc8949b256c2cdfce7f9d
Miguel Duarte <mduarte@10pines.com>
2016-04-01 16:45:29 -0300

    Agregadas operaciones CRUD para Topic.

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,33 @@
+class TopicsController < ApplicationController
+
+  def create
+    topic = Topic.create!(create_params)
+    render json: topic
+  end
+
+  def index
+    topics = Topic.all
+    render json: topics
+  end
+
+  def show
+    topic = Topic.find(params[:id])
+    render json: topic
+  end
+
+  def update
+    topic = Topic.find(parms[:id])
+    topic.update(update_params)
+    render json: topic
+  end
+
+  private
+
+  def create_params
+    params.require(:topic).permit(:title, :body, :speaker_id)
+  end
+
+  def update_params
+    params.require(:topic).permit(:title, :body)
+  end
+end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,5 @@
 class Topic < ActiveRecord::Base
+  validates_presence_of :title, :body, :speaker
+
+  belongs_to :speaker, class_name: 'User'
 end
